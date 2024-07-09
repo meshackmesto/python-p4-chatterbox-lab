43611324b1776129c66faec00e977a17da55d8da
meshackmesto <meshackorina93@gmail.com>
2024-07-09 18:35:29 +0300

    nn

---
+++ server/migrations/versions/91b1b6757139_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 91b1b6757139
+Revises: d8f45278abe8
+Create Date: 2024-07-09 18:33:03.078765
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '91b1b6757139'
+down_revision = 'd8f45278abe8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
